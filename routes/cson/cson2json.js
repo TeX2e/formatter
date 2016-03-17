a5d91cc0f20021f77fed50ab665ddb32a24aed4b
Mako <mnfeconicu41@gmail.com>
2016-03-17 19:46:01 +0900

    fix a bug to display error

--- routes/cson/cson2json.js
+++ routes/cson/cson2json.js
@@ -8,7 +8,9 @@ function convertCSONtoJSON(csonText) {
     var jsonObj = CSON.parse(csonText);
 
     if (jsonObj instanceof Error) {
-        return result.stack;
+        var error = jsonObj;
+        return error.toString()
+            .replace(/\033\[.*?m/g, ''); // remove special charactor on terminal
     }
     return JSON.stringify(jsonObj, null, "\t");
 }
