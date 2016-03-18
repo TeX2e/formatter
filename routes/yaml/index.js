488a8afcb8df4c34beb086286537e8722d268c50
Mako <mnfeconicu41@gmail.com>
2016-03-18 11:48:37 +0900

    more general template

--- routes/yaml/index.js
+++ routes/yaml/index.js
@@ -4,9 +4,21 @@ var router = express.Router();
 
 var yaml2json = require('./yaml2json.js');
 
+var sampleText = `
+str: foobar
+num: 123
+bool: true
+array:
+  - spam
+  - ham
+dict:
+  prop: foo
+  prop2: 123
+`;
+
 // GET /convert/yaml2json
 router.get('/convert/yaml2json', function (req, res) {
-    res.render('yaml/yaml2json', { title: 'YAML formatter' });
+    res.render('yaml/yaml2json', { title: 'YAML formatter', textFrom: sampleText });
 });
 
 // POST /convert/yaml2json
