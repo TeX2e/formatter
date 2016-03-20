5992bacdffe87562b43bad9081e56e7f94b90feb
Mako <mnfeconicu41@gmail.com>
2016-03-20 22:44:51 +0900

    add coffee option: --bare

--- routes/coffee/coffee2js.js
+++ routes/coffee/coffee2js.js
@@ -4,7 +4,7 @@ var coffee = require('coffee-script');
 // convert coffee to js
 function convertCoffeeToJS(coffeeText) {
     try {
-        return coffee.compile(coffeeText);
+        return coffee.compile(coffeeText, { bare: true } );
     } catch (e) {
         return e.toString()
             .replace(/\033\[.*?m/g, ''); // remove special charactor on terminal
