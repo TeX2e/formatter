35ddd26c0e9957bfc4b2bc55a0177d8af6935160
Mako <mnfeconicu41@gmail.com>
2016-03-22 21:04:19 +0900

    update test's helper method

--- test/helper.js
+++ test/helper.js
@@ -1,31 +1,41 @@
 
+// string.scan
+String.prototype.scan = function(regex) {
+  if (!regex.global) throw "regex must have 'global' flag set";
+  var r = []
+  this.replace(regex, function() {
+    r.push(Array.prototype.slice.call(arguments, 1, -2));
+  });
+  return r;
+}
+
+// array.flatten
+Array.prototype.flatten = function() {
+  return Array.prototype.concat.apply([], this);
+};
+
+// array.max
+Array.prototype.max = function() {
+  return Math.max.apply(null, this);
+};
+
+// array.min
+Array.prototype.min = function() {
+  return Math.min.apply(null, this);
+};
+
+
 // strip indentation
 module.exports.dedent = function (string) {
-  var lines = string
-    .trimLeft()   // dedent eats leading whitespace too
-    .split("\n");
+  var margin = string
+    .scan(/^( +)/gm)
+    .flatten()
+    .map(line => line.length)
+    .min();
+  
+  var result = string
+    .replace(new RegExp(`^ {${margin}}`, 'gm'), '')
+    .replace(/^\n/, '');
 
-  // detect mindent (minimum indent)
-  var mindent = null;
-  lines.forEach(line => {
-    var m = line.match(/^ +/);
-    if (m) {
-      var indent = m[0].length;
-      if (!mindent) {
-        // this is the first indented line
-        mindent = indent;
-      } else {
-        mindent = Math.min(mindent, indent);
-      }
-    }
-  });
-
-  // strip indentation
-  if (mindent !== null) {
-    return lines
-      .map(line => line[0] === " " ? line.slice(mindent) : line)
-      .join("\n");
-  } else {
-    return lines.join("\n");
-  }
+  return result;
 }
